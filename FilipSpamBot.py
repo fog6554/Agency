926102d9400192fbaaaecffa12aad76f9730f004
Tomáš Andrus <andruto@student.uzlabina.cz>
2022-11-11 10:29:35 +0100

    filip

---
+++ FilipSpamBot.py
@@ -0,0 +1,27 @@
+import os 
+from pynput.keyboard import Key, Controller
+import pyautogui
+import time
+import keyboard
+time.sleep(5)
+
+for i in range(100):
+    keyboard.write('Umri')
+    pyautogui.press("tab")
+    pyautogui.press("tab")
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    
+    #pyautogui.press("down")
+    #pyautogui.press("down")
+
+    
+    #pyautogui.press("enter")
+ 
+    with pyautogui.hold('shift'):
+        pyautogui.press("tab")
+        pyautogui.press("tab")
+        pyautogui.press("tab")
+        
+    
